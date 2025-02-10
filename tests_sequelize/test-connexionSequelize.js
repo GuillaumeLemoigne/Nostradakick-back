460626b8a2a0443495842902795d8bb5c538dc41
Fabien Gensane <fabiengensane@MacBook-Pro-de-Fabien.local>
2025-02-10 21:41:51 +0100

    Test Model Match ok

--- tests_sequelize/test-connexionSequelize.js
+++ tests_sequelize/test-connexionSequelize.js
@@ -1,4 +1,4 @@
-import {sequelize} from './src/sequelize.js';
+import {sequelize} from '../src/sequelize.js';
 
 async function testConnection() {
   try {
---
+++ tests_sequelize/test-match.js
@@ -0,0 +1,44 @@
+import {Match} from "../src/models/Match.js"
+
+
+// * === CRUD DE LA TABLE competition ===
+
+// *** CREATE ***
+
+// 1 - Création d'une nouvelle entrée dans la table match
+async function createMatch () {
+    try {
+        const newMatch = await Match.create({ 
+            competition_id: "1",
+            date: "2026-11-18T20:30:00Z",
+            stadium:"jojo",
+         });
+         console.log(JSON.stringify(newMatch, null, 2));
+         
+    } catch (error) {
+        console.error("Ouuuppppps, ya comme un hic!")
+    }
+};
+// createMatch();
+
+// *** DELETE ***
+
+// 2 - Suppression d'une entrée dans la table match
+
+async function deleteMatch () {
+    try {
+        const matchDelete = await Match.destroy(
+            {
+                where: {
+                    date: "2026-11-18T20:30:00Z",
+                },
+            }
+        );
+        // On affiche le résultat
+        console.log(JSON.stringify(matchDelete, null, 2));
+    } catch (error) {
+        console.error("Ouuuppppps, ya comme un hic!")
+    }
+};
+
+deleteMatch()
