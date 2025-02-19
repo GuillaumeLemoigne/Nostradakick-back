1f9ae3c29ce77bf6880b252b5fb785e83edf3d81
Fabien Gensane <fabiengensane@MacBook-Pro-de-Fabien.local>
2025-02-19 15:08:18 +0100

    commit pour pull

--- src/routers/match.router.js
+++ src/routers/match.router.js
@@ -7,10 +7,10 @@ const matchRouter = Router();
 matchRouter.get("/api/matchs", matchController.getAllMatch);
 
 // Les routes de notre API CRUD de gestion des résultats des matchs de football passés
-matchRouter.get("/api/results");
+matchRouter.get("/api/results", authHandler, matchController.getEndedMatch);
 
 // La routes de notre API de consultation du calendrier des matchs à venir dans la compétition
-matchRouter.get("/api/calendar");
+matchRouter.get("/api/calendar",  matchController.getUpcomingMatch);
 
 // La route de notre API de consultation des informations (principalement le résultat) des matchs de football en cours
 matchRouter.get("/api/inplay");
